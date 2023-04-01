8e47522e7b9359f4fa64f1fc9cc8a91c73ef2c90
furkanGencogullari <102527257+furkanGencogullari@users.noreply.github.com>
2023-04-01 09:39:10 +0300

    Setup Project

---
+++ TapBat/GameScene.swift
@@ -0,0 +1,51 @@
+//
+//  GameScene.swift
+//  TapBat
+//
+//  Created by Furkan Gençoğulları on 1.04.2023.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+
+    }
+    
+    
+    func touchDown(atPoint pos : CGPoint) {
+
+    }
+    
+    func touchMoved(toPoint pos : CGPoint) {
+
+    }
+    
+    func touchUp(atPoint pos : CGPoint) {
+
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        
+    }
+}
